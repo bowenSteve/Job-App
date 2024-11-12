965b87220121c36ceb8e24b58923b6f2664b8e4b
stephenbowen <bowensteven17@gmail.com>
2024-11-12 10:15:35 +0300

    updates on the frontend

--- server/app.py
+++ server/app.py
@@ -13,13 +13,18 @@ from models import db, JobPosting
 migrate = Migrate(app, db)
 db.init_app(app)
 
-@app.route('/get_jobpostings', methods=['POST'])
+@app.route('/get_jobpostings', methods=['GET'])
 def get_jobpostings():
     jobs = JobPosting.query.all()
     jobs_list = [job.to_dict() for job in jobs]
-    return jsonify(jobs_list)
+    return jsonify(jobs_list), 201
 
-
+@app.route('/job/<int:job_id>', methods=['GET'])
+def get_job_id(job_id):
+    job = JobPosting.query.get(job_id)
+    if not job:
+        return jsonify({'error': 'Job not found'}), 404
+    return jsonify(job.to_dict()), 201
 
 if __name__  == '__main__':
     app.run(debug = True)
